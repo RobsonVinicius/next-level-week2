4a0832fa411ac9c4d4ba5ad5aea63121d6ad73a2
Robson Vinicius <robsondgn@outlook.com>
2020-09-22 10:41:54 -0300

    Adicionando script no addField.js para limpar os valores dos campos ao duplicar

--- scripts/addField.js
+++ scripts/addField.js
@@ -4,11 +4,20 @@ document.querySelector("#add-time")
 .addEventListener('click', cloneField)
 
 // Executar uma ação
-function cloneField() {
-  console.log("cheguei aqui")
+function cloneField() {  
   // Duplicar os campos. Que campos?
-  const fields = document.querySelector('.schedule-item').cloneNode(true)
+  const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+  // Pegar os campos para limpar a informação. Que campos? 
+  const fields = newFieldContainer.querySelectorAll('input')
+
+  // Para cada campo, limpar
+  fields.forEach(function(field) {
+    // Pegar o field do momento e limpa
+    field.value = ""
+  })
+
   // Colocar na página. Onde?
-  document.querySelector('#schedule-items').appendChild(fields)
+  document.querySelector('#schedule-items').appendChild(newFieldContainer)
 
 }
