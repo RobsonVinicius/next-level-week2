27fa26a0be4a16e890ac65f7dad978b4291561d9
Robson Vinicius <robsondgn@outlook.com>
2020-09-22 10:05:17 -0300

    Criando a lógica para duplicar o formulário dos Horários Disponíveis da página Dar Aulas

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,1 +1,14 @@
-document.querySelector('#container').style.backgroundColor = "black"
+// Procurar o botao 
+document.querySelector("#add-time")
+// Quando clicar no botao
+.addEventListener('click', cloneField)
+
+// Executar uma ação
+function cloneField() {
+  console.log("cheguei aqui")
+  // Duplicar os campos. Que campos?
+  const fields = document.querySelector('.schedule-item').cloneNode(true)
+  // Colocar na página. Onde?
+  document.querySelector('#schedule-items').appendChild(fields)
+
+}
